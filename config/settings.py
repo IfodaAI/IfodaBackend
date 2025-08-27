dfcba1e3e5a872c656569297b162040e0d0b6c55
ㅤshokh <galagraft666@gmail.com>
2025-08-27 10:51:48 +0500

    Added gunicorn and dotenv to requirments.txt

--- config/settings.py
+++ config/settings.py
@@ -19,8 +19,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_DOMAINS").split(",")
-CORS_ALLOWED_ORIGINS = os.environ.get("ALLOWED_DOMAINS").split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_DOMAINS", [])
+CORS_ALLOWED_ORIGINS = os.environ.get("ALLOWED_DOMAINS", [])
 
 # Application definition
 
