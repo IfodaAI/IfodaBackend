2ae5ba4c394a8bcfb46541cc49b79934cfa70d8e
ㅤshokh <galagraft666@gmail.com>
2025-08-27 17:06:45 +0500

    Added static files support

--- config/settings.py
+++ config/settings.py
@@ -70,7 +70,8 @@ PHONENUMBER_DB_FORMAT = "E164"  # Forces phone number inputs such as "9989012345
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static_collected")
+STATICFILES_DIRS = [BASE_DIR / "static"] 
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
