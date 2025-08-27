cf660b269663f6522d03cd15a9d424fadfd6890e
ㅤshokh <galagraft666@gmail.com>
2025-08-27 11:09:13 +0500

    Env fix attempt

--- config/settings.py
+++ config/settings.py
@@ -3,11 +3,13 @@ from pathlib import Path
 from datetime import timedelta
 import environ
 
-# Load environment variables from .env file
-env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Load environment variables from .env file
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
