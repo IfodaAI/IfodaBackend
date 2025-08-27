742a2b3c3f3142d74208048da3312aa189afec05
ㅤshokh <galagraft666@gmail.com>
2025-08-27 11:23:14 +0500

    Env fix attempt

--- config/settings.py
+++ config/settings.py
@@ -15,11 +15,9 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.list("SECRET_KEY")
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.list("DEBUG", default=False)
-
+# Don't run with debug turned on in production!
+SECRET_KEY = env.str("SECRET_KEY")
+DEBUG = env.bool("DEBUG", default=False)
 ALLOWED_HOSTS = env.list("ALLOWED_DOMAINS", default=["api.ifoda-shop.uz"])
 CORS_ALLOWED_ORIGINS = env.list("ALLOWED_DOMAINS", default=[])
 
