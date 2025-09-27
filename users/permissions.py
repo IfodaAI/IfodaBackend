da1e7a132da8aafffd08f2862469b77f879bbb93
Bekzod4606 <bdmbzd99m27z259p@studenti.unime.it>
2025-09-27 10:14:29 +0500

    TelegramUserViewSet update

---
+++ users/permissions.py
@@ -0,0 +1,22 @@
+from rest_framework.permissions import BasePermission
+
+
+class PostAndCheckUserOnly(BasePermission):
+    """
+    Faqat:
+      - POST (create) ruxsat
+      - check_user (GET action) ruxsat
+      Qolganlariga taqiq
+    """
+
+    def has_permission(self, request, view):
+        # check_user action faqat GET da ruxsat
+        if hasattr(view, "action") and view.action == "check_user":
+            return request.method == "GET"
+
+        # Faqat POST ga ruxsat
+        if request.method == "POST":
+            return True
+
+        # Qolgan hamma deny
+        return False
--- users/views.py
+++ users/views.py
@@ -1,5 +1,10 @@
+from rest_framework.exceptions import ValidationError
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import AllowAny
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.request import Request
+from django.http import HttpRequest
+from .permissions import PostAndCheckUserOnly
 
 from .models import User, TelegramUser, Branch
 from .serializers import UserSerializer, TelegramUserSerializer, BranchSerializer
@@ -11,9 +16,19 @@ class UserViewSet(ModelViewSet):
 class TelegramUserViewSet(ModelViewSet):
     queryset = TelegramUser.objects.all()
     serializer_class = TelegramUserSerializer
-    permission_classes=[AllowAny]
-    pagination_class=None
-    filterset_fields=["telegram_id"]
+    permission_classes=[PostAndCheckUserOnly]
+    
+    @action(detail=False, methods=["get"])
+    def check_user(self, request:HttpRequest|Request):
+        telegram_id=request.GET.get("telegram_id")
+        if not telegram_id:
+            raise ValidationError({"telegram_id": "Bu maydon majburiy."})
+        queryset = self.filter_queryset(self.get_queryset())
+        instance=queryset.filter(telegram_id=telegram_id)
+        if not instance:
+            return Response({})
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
 
 class BranchViewSet(ModelViewSet):
     queryset = Branch.objects.all()
