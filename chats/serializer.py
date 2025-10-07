7859070a0b4c188c8b69851c51de2482cbee02f7
Bekzod4606 <bdmbzd99m27z259p@studenti.unime.it>
2025-10-07 14:08:14 +0500

    Some updates

--- chats/serializer.py
+++ chats/serializer.py
@@ -1,11 +1,18 @@
 from utils.serializers import BaseModelSerializer
 from .models import Room, Message
-
+from django.http import HttpRequest
 
 class RoomSerializer(BaseModelSerializer):
     class Meta(BaseModelSerializer.Meta):
         model = Room
 
+    def __init__(self, *args, **kwargs):
+        super(RoomSerializer, self).__init__(*args, **kwargs)
+        request: HttpRequest = self.context.get("request")
+        if request and request.method == "GET":
+            messages = request.GET.get("messages")
+            if messages == "true":
+                self.fields["messages"] = MessageSerializer(context=self.context,many=True)
 
 class MessageSerializer(BaseModelSerializer):
     class Meta(BaseModelSerializer.Meta):
