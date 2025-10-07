b4d56da7b647bf3f25800ea646e71002eb9de3c7
Bekzod4606 <bdmbzd99m27z259p@studenti.unime.it>
2025-10-07 13:54:00 +0500

    Some updates

--- chats/views.py
+++ chats/views.py
@@ -1,4 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.request import Request
+from rest_framework.permissions import IsAuthenticated
+from django.http import HttpRequest
 
 from .models import Room, Message
 from .serializer import RoomSerializer, MessageSerializer
@@ -6,6 +11,16 @@ from .serializer import RoomSerializer, MessageSerializer
 class RoomViewSet(ModelViewSet):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+    permission_classes=[IsAuthenticated]
+
+    def create(self, request:HttpRequest|Request, *args, **kwargs):
+        data=request.data
+        data["owner"]=request.user.id
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 class MessageViewSet(ModelViewSet):
     queryset = Message.objects.all()
