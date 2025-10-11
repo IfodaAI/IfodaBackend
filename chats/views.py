cabc587025d14c69e3bd11a377126f55390f3826
Bekzod4606 <bdmbzd99m27z259p@studenti.unime.it>
2025-10-11 14:20:15 +0500

    Some updates

--- chats/views.py
+++ chats/views.py
@@ -17,7 +17,7 @@ class RoomViewSet(ModelViewSet):
 
     def create(self, request:HttpRequest|Request, *args, **kwargs):
         data=request.data
-        data["owner"]=request.user.id
+        data["owner"]=self.request.user.id
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
