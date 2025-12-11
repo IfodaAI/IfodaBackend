2484180ad176d1adb192f0d30ae8164a849d283f
Bekzod4606 <bdmbzd99m27z259p@studenti.unime.it>
2025-12-11 14:15:25 +0500

    some updates

--- chats/views.py
+++ chats/views.py
@@ -1,10 +1,67 @@
+# from rest_framework.viewsets import ModelViewSet
+# from rest_framework.response import Response
+# from rest_framework import status
+# from rest_framework.request import Request
+# from rest_framework.permissions import IsAuthenticated
+# from rest_framework.decorators import action
+# from django.db.models import Subquery, OuterRef, DateTimeField
+# from django.http import HttpRequest
+# from asgiref.sync import async_to_sync
+# from channels.layers import get_channel_layer
+
+# from .models import Room, Message
+# from .serializer import RoomSerializer, MessageSerializer
+# from .ai import generate_prompt
+# from products.models import Disease
+# from products.serializers import DiseaseSerializer
+
+# class RoomViewSet(ModelViewSet):
+#     queryset = Room.objects.all()
+#     serializer_class = RoomSerializer
+#     permission_classes=[IsAuthenticated]
+
+#     def create(self, request:HttpRequest|Request, *args, **kwargs):
+#         data=request.data.copy()
+#         data["owner"]=request.user.id
+#         serializer = self.get_serializer(data=data)
+#         serializer.is_valid(raise_exception=True)
+#         self.perform_create(serializer)
+#         headers = self.get_success_headers(serializer.data)
+#         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    
+#     # def get_queryset(self):
+#     #     user = self.request.user
+
+#     #     # If user's role is admin, send all orders
+#     #     if user.is_superuser or user.role == "ADMIN":
+#     #         return Room.objects.all()
+
+#     #     # If it's normal user, send only only their own orders.
+#     #     return Room.objects.filter(owner=user)
+#     def get_queryset(self):
+#         user = self.request.user
+
+#         last_message_sub = Message.objects.filter(
+#             room=OuterRef("pk")
+#         ).order_by("-created_date")
+
+#         qs = Room.objects.annotate(
+#             last_message_time=Subquery(
+#                 last_message_sub.values("created_date")[:1],
+#                 output_field=DateTimeField()
+#             )
+#         ).select_related("owner").order_by("-last_message_time")
+
+#         if user.is_superuser or getattr(user, "role", None) == "ADMIN":
+#             return qs
+
+#         return qs.filter(owner=user)
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.request import Request
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.decorators import action
-from django.db.models import Subquery, OuterRef, DateTimeField
+from django.db.models import Subquery, OuterRef, DateTimeField, F
 from django.http import HttpRequest
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
@@ -15,48 +72,46 @@ from .ai import generate_prompt
 from products.models import Disease
 from products.serializers import DiseaseSerializer
 
+
 class RoomViewSet(ModelViewSet):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
-    permission_classes=[IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
-    def create(self, request:HttpRequest|Request, *args, **kwargs):
-        data=request.data.copy()
-        data["owner"]=request.user.id
+    def create(self, request: HttpRequest | Request, *args, **kwargs):
+        data = request.data.copy()
+        data["owner"] = request.user.id
         serializer = self.get_serializer(data=data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-    
-    # def get_queryset(self):
-    #     user = self.request.user
 
-    #     # If user's role is admin, send all orders
-    #     if user.is_superuser or user.role == "ADMIN":
-    #         return Room.objects.all()
-
-    #     # If it's normal user, send only only their own orders.
-    #     return Room.objects.filter(owner=user)
     def get_queryset(self):
         user = self.request.user
 
+        # Subquery: oxirgi message vaqti
         last_message_sub = Message.objects.filter(
             room=OuterRef("pk")
         ).order_by("-created_date")
 
+        # Roomlarni annotate qilamiz: last_message_time
         qs = Room.objects.annotate(
             last_message_time=Subquery(
                 last_message_sub.values("created_date")[:1],
                 output_field=DateTimeField()
             )
-        ).select_related("owner").order_by("-last_message_time")
+        ).select_related("owner").order_by(
+            F('last_message_time').desc(nulls_last=True)  # NULL bo‘lsa oxiriga tushadi
+        )
 
+        # Admin barcha roomlarni ko‘radi
         if user.is_superuser or getattr(user, "role", None) == "ADMIN":
             return qs
 
+        # Oddiy user faqat o‘zining roomlari
         return qs.filter(owner=user)
-    
+
 class MessageViewSet(ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
