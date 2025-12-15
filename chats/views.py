da8d195f8cfc1c297c9a5385630caf541b06b073
Bekzod4606 <bdmbzd99m27z259p@studenti.unime.it>
2025-12-15 11:41:45 +0500

    some updates

--- chats/views.py
+++ chats/views.py
@@ -88,9 +88,9 @@ class MessageViewSet(ModelViewSet):
                 f'chat_{room.id}',
                 {"type": "chat_message", "message": payload},
             )
-        async_to_sync(channel_layer.group_send)(
-            "notifications", {"type": "notify", "message": {"message": "Yangi bildirishnoma qabul qilindi!"}}
-        )
+        # async_to_sync(channel_layer.group_send)(
+        #     "notifications", {"type": "notify", "message": {"message": "Yangi bildirishnoma qabul qilindi!"}}
+        # )
         # TODO
         send_telegram_message(chat_id="1330892088",text="🔔 Yangi bildirishnoma qabul qilindi!")
         return Response(serializer.data, status=status.HTTP_201_CREATED)
