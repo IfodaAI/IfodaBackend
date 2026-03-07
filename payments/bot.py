0e79b41b8e1805c82a0bfa34027ebec2724f006d
Ilyosxon0000 <mahmudovilyosxon9@gmail.com>
2026-03-07 07:13:21 +0500

    update payment

--- payments/bot.py
+++ payments/bot.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 def send_payment_success_message(telegram_id):
     """To'lov muvaffaqiyatli bo'lganda foydalanuvchiga Telegram xabar yuboradi."""
     text = (
-        "To'lov muvaffaqiyatli amalga oshildi ✅ \n"
+        "To'lov muvaffaqiyatli amalga oshirildi ✅ \n"
         "Buyurtma 24 soat ichida yetkazib beriladi 🚕\n\n"
         "🌱 <b>IFODA</b> - <b>Birgalikda yetishtiramiz!</b>"
     )
